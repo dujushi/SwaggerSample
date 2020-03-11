44743208a407657bd40a9dde0b30200108f38995
William Guo <william.guo@plexure.com>
2020-03-12 11:39:42 +1300

    Add security requirement

--- SwaggerSample/Startup.cs
+++ SwaggerSample/Startup.cs
@@ -27,6 +27,31 @@ namespace SwaggerSample
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
 
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme."
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        }, 
+                        new string[] {}
+                    }
+                });
+
                 var filePath = Path.Combine(AppContext.BaseDirectory, "SwaggerSample.xml");
                 c.IncludeXmlComments(filePath);
             });
