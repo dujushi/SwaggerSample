2ec45f3ac1082abf374d37b2cc82931441c47933
William Guo <william.guo@plexure.com>
2020-03-13 23:12:47 +1300

    Add InternalServerErrorResponseOperationFilter

---
+++ SwaggerSample/Swagger/OperationFilters/InternalServerErrorResponseOperationFilter.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace SwaggerSample.Swagger.OperationFilters
+{
+    /// <summary>
+    /// This operation filter adds internal server error response.
+    /// </summary>
+    public class InternalServerErrorResponseOperationFilter : IOperationFilter
+    {
+        /// <inheritdoc />
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            operation.Responses.Add(((int)HttpStatusCode.InternalServerError).ToString(), new OpenApiResponse { Description = HttpStatusCode.InternalServerError.ToString() });
+        }
+    }
+}
